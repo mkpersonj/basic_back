13f0d25899dc9c3b9680b0ca654194012ce00b00
mkpersonj <it.routravel@gmail.com>
2023-02-19 13:09:42 +0900

    비동기 오류처리 middleware 생성

--- app.js
+++ app.js
@@ -64,13 +64,11 @@ app.use(function (req, res, next) {
 
 // error handler
 app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render("error");
+  // return err message
+  console.log(err);
+  res.status(err.status || 500).send({
+    err: req.app.get("env") === "production" ? null : err.message,
+  });
 });
 
 app.listen(process.env.BACK_PORT, function () {
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+const { asyncWrap } = require("../middleware/common");
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+// 동기호출
+router.get("/syncErrorTest", (req, res) => {
+  return res.send(data);
 });
 
+// 비동기호출
+router.get(
+  "/asyncErrorTest",
+  asyncWrap(async (req, res) => {
+    return res.send(data);
+  })
+);
 module.exports = router;
